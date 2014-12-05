4f1c3114042274bfe5c21db003fae8ce11823ad6
Michael Feliciano <mfeliciano.enrique@gmail.com>
2014-12-04 22:48:52 -0500

    Re Organized code,changed theme, added links between pages

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,6 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
+angular.module('DayByDay', ['ionic'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -13,9 +9,42 @@ angular.module('starter', ['ionic'])
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
+})
+
+
+.controller("LoginController",function(){
+  this.username = "uName";
+  this.password = "pWord";
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+    .state('page5', {
+      url: '/login',
+      templateUrl: 'page5.html'
+    })
+    
+    .state('page6', {
+      url: '/signup',
+      templateUrl: 'page6.html'
+    })
+    
+    .state('page8', {
+      url: '/tabs',
+      templateUrl: 'page8.html'
+    })
+    ;
+  // if none of the above states are matched, use this as the fallback
+  
+  $urlRouterProvider.otherwise('/login');
+  
 });
